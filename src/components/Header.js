3aba5bc75457e2289f4bc5751c9e7a19ef9daa9e
Sahil Mody <sahilmody1234@gmail.com>
2022-11-15 23:37:25 -0500

    Partially Updated

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+
+export default function Header(){
+    return (
+        <header className="header">
+            <img className="header-image"
+            src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
+            alt="Problem?"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header-project">React Course - Project 3</h4>
+        </header>
+    )
+}
